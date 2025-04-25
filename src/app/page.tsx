30dc02c03a92168d6c5a1c346d41299c224153ec
inthhh <glass005533@gmail.com>
2025-04-25 23:07:35 +0900

    feat: Home에서 Login 페이지로 라우팅 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
 import style from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/login");
+  };
+
   return (
     <div>
-      <div className={style.wrapper}>
+      <div className={style.wrapper} onClick={handleClick}>
         <p>주문하시려면 터치하세요.</p>
       </div>
     </div>
