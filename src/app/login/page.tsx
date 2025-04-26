a09c5f8c72dc494cc0b991a0f5d3ec049759401c
inthhh <glass005533@gmail.com>
2025-04-26 16:53:16 +0900

    feat: 로그인 컴포넌트 요소 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import style from "../page.module.css";
+import { useRouter } from "next/navigation";
+
 export default function Login() {
+  const router = useRouter();
+  const handleClientLogin = () => {
+    router.push("/menu");
+  };
   return (
-    <div>
-      <h1>로그인 페이지</h1>
+    <div className={style.wrapper}>
+      <form className="form-wrapper">
+        <label>아이디</label>
+        <input type="text" name="username" />
+        <br />
+        <label>비밀번호</label>
+        <input type="password" name="password" />
+        <br />
+        <button type="button">취소</button>
+        <button type="button">로그인</button>
+        <button type="button" onClick={handleClientLogin}>
+          구글 로그인
+        </button>
+      </form>
     </div>
   );
 }
