7284d406eec9e5475ec2d0449b638d181ace7a96
inthhh <glass005533@gmail.com>
2025-04-26 20:50:28 +0900

    feat: 주문확인(/client/check-order) 라우팅 연결

---
+++ src/app/client/check-order/page.tsx
@@ -0,0 +1,7 @@
+export default function CheckOrder() {
+  return (
+    <div>
+      <h1>주문을 확인하세요.</h1>
+    </div>
+  );
+}
--- src/app/client/menu/menu_footer.tsx
+++ src/app/client/menu/menu_footer.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 import style from "./style.module.css";
+import { useRouter } from "next/navigation";
 
 const Menu_Footer = () => {
+  const router = useRouter();
+  const handleSubmit = () => {
+    router.push("/client/check-order");
+  };
+  const handelCancel = () => {
+    router.push("/");
+  };
   return (
     <div className={style.footer_wrapper}>
       <div>주문 내역</div>
       <div className={style.order_list}>(주문 리스트)</div>
       <button>비우기</button>
       <div className={style.button_wrapper}>
-        <button className={style.button}>주문 취소</button>
-        <button className={style.button}>주문 완료</button>
+        <button className={style.button} onClick={handelCancel}>
+          주문 취소(처음으로)
+        </button>
+        <button className={style.button} onClick={handleSubmit}>
+          주문 완료
+        </button>
       </div>
     </div>
   );
