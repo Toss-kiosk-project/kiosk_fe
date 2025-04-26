cde0728c4d1183a97bb6c423fa4dd01ae867f10a
inthhh <glass005533@gmail.com>
2025-04-26 21:46:52 +0900

    feat: client layout 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import style from "./page.module.css";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Logo from "./images/logo.png";
 
 export default function Home() {
   const router = useRouter();
@@ -11,7 +13,10 @@ export default function Home() {
   return (
     <div>
       <div className={style.wrapper} onClick={handleClick}>
-        <p>주문하시려면 터치하세요.</p>
+        <Image src={Logo} alt="logo" width={200} height={200} />
+        <br />
+        <br />
+        <p>시작하려면 터치하세요.</p>
       </div>
     </div>
   );
