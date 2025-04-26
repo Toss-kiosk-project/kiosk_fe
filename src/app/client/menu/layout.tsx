ebe803f749f4e146a5b0699062abfa829006b7c6
inthhh <glass005533@gmail.com>
2025-04-26 20:33:06 +0900

    feat: menu 페이지 header/footer 요소 추가

--- src/app/client/menu/layout.tsx
+++ src/app/client/menu/layout.tsx
@@ -1,3 +1,5 @@
+import style from "./style.module.css";
+
 export default function MenuLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,19 @@ export default function MenuLayout({
 }>) {
   return (
     <div>
+      <div className={style.header_wrapper}>
+        Header <button>이전</button>
+      </div>
       <div>{children}</div>
+      <div className={style.footer_wrapper}>
+        <div>주문 내역</div>
+        <div className={style.order_list}>(주문 리스트)</div>
+        <button>비우기</button>
+        <div className={style.button_wrapper}>
+          <button className={style.button}>주문 취소</button>
+          <button className={style.button}>주문 완료</button>
+        </div>
+      </div>
     </div>
   );
 }
