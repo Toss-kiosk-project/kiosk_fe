a13f1dc7ef39bf31313fe2f668a399df6a4239e0
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-04-26 22:25:06 +0900

    feat: admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,11 +1,21 @@
+import styles from "./admin-layout.module.css";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <div style={{ width: "1200px", height: "760px", backgroundColor: "white" }}>{children}</div>
+    <div className={styles.wrapper}>
+      <div className={styles.title}>키오스크 관리자 화면</div>
+      <div className={styles.layoutContainer}>
+        <div className={styles.sidebar}>
+          <div className={styles.menuItem}>회원관리</div>
+          <div className={styles.menuItem}>상품관리</div>
+          <div className={styles.menuItem}>주문관리</div>
+        </div>
+        {children}
+      </div>
     </div>
   );
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,3 @@
 export default function Admin() {
-  return (
-    <div>
-      <h1>admin</h1>
-    </div>
-  );
+  return <div></div>;
 }
